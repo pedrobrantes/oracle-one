2b5b29c8573cc5df6d961d76b06e5f7a3f1cc6f4
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-10 15:47:33 -0300

    feat: add else block to handle wrong guess

--- js-logic-initial-project/app.js
+++ js-logic-initial-project/app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto (5)');
+    alert('Isso aí! Você descobriu o número secreto (5)');
+} else {
+    alert('Você errou :(');
 }
