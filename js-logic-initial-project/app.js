be5cd4738d64565f3754a5e08565d4d3253df05c
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-10 18:15:41 -0300

    feat: add while loop for multiple guesses

--- js-logic-initial-project/app.js
+++ js-logic-initial-project/app.js
@@ -1,16 +1,17 @@
 alert('Boas-vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log(`Número secreto: ${numeroSecreto}`);
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log(`Chute do usuário: ${chute}`);
+console.log(numeroSecreto);
+let chute;
 
-if (chute == numeroSecreto) {
-    console.log('Condição IF satisfeita.');
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
     } else {
-        alert(`O número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
     }
 }
