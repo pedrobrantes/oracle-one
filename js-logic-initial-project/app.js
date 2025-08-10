266aeb638e8e0c4ca08165176a6646ff718759ee
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-10 18:05:48 -0300

    feat: add hints for higher or lower guesses

--- js-logic-initial-project/app.js
+++ js-logic-initial-project/app.js
@@ -1,5 +1,5 @@
 alert('Boas-vindas ao jogo do número secreto');
-let numeroSecreto = 4;
+let numeroSecreto = 5;
 console.log(`Número secreto: ${numeroSecreto}`);
 let chute = prompt('Escolha um número entre 1 e 10');
 console.log(`Chute do usuário: ${chute}`);
@@ -8,6 +8,9 @@ if (chute == numeroSecreto) {
     console.log('Condição IF satisfeita.');
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    console.log('Condição IF não satisfeita. Entrando no ELSE.');
-    alert('Você errou :(');
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
 }
