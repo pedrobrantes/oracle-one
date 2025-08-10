b6d6bdfc3cc8785876accc5adedf1a1a89ef31ce
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-10 15:55:48 -0300

    feat: add solutions to challenges and update name and age

---
+++ js-logic-initial-project/desafios.js
@@ -0,0 +1,32 @@
+// Desafio 1
+alert('Boas vindas ao nosso site!');
+
+// Desafio 2
+let nome = 'Pedro';
+
+// Desafio 3
+let idade = null;
+
+// Desafio 4
+let numeroDeVendas = 50;
+
+// Desafio 5
+let saldoDisponivel = 1000;
+
+// Desafio 6
+alert('Erro! Preencha todos os campos');
+
+// Desafio 7
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+// Desafio 8
+let nomeUsuario = prompt('Qual o seu nome?');
+
+// Desafio 9
+let idadeUsuario = prompt('Qual a sua idade?');
+
+// Desafio 10
+if (idadeUsuario >= 18) {
+    alert('Pode tirar a habilitação!');
+}
