fd2276eb3991f7ccef5af2010c5ffe9a52e91f39
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-10 16:33:20 -0300

    feat: add more console.log statements for debugging

--- js-logic-initial-project/app.js
+++ js-logic-initial-project/app.js
@@ -1,10 +1,13 @@
 alert('Boas-vindas ao jogo do número secreto');
 let numeroSecreto = 4;
-console.log(numeroSecreto);
+console.log(`Número secreto: ${numeroSecreto}`);
 let chute = prompt('Escolha um número entre 1 e 10');
+console.log(`Chute do usuário: ${chute}`);
 
 if (chute == numeroSecreto) {
+    console.log('Condição IF satisfeita.');
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
+    console.log('Condição IF não satisfeita. Entrando no ELSE.');
     alert('Você errou :(');
 }
