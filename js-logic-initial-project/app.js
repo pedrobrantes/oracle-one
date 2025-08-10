1914853e664ff2de5b30f39de0e42b0f023c2422
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-10 16:01:31 -0300

    feat: add console.log to show secret number

--- js-logic-initial-project/app.js
+++ js-logic-initial-project/app.js
@@ -1,5 +1,6 @@
 alert('Boas-vindas ao jogo do número secreto');
 let numeroSecreto = 5;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
