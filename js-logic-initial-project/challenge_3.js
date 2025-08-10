ad26bf34cab5d0bdfeea163e17f1eb19188e6380
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2025-08-10 18:49:24 -0300

    feat: add solutions to challenge 3

---
+++ js-logic-initial-project/challenge_3.js
@@ -0,0 +1,28 @@
+// Desafio 1
+let contador = 1;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+
+// Desafio 2
+contador = 10;
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
+
+// Desafio 3
+let numero = prompt('Digite um número para a contagem regressiva:');
+while (numero >= 0) {
+    console.log(numero);
+    numero--;
+}
+
+// Desafio 4
+numero = prompt('Digite um número para a contagem progressiva:');
+contador = 0;
+while (contador <= numero) {
+    console.log(contador);
+    contador++;
+}
